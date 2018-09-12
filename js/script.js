2d4a1c8e483cc67563f35cb5f2ac4bddec8103e3
sid <siddhartpai@gmail.com>
2018-09-12 11:43:38 +0530

    Changed styles, added stop button, added rules for life

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 (function() {
     
-    var A  = [
-        [1,1,1],
-        [0,1,0],
-        [0,0,0]
-    ]
-
 function GameOfLife(size){
     this.size = size;
     this.golArr = [];
@@ -16,10 +10,10 @@ GameOfLife.prototype.initialize = function(){
         var row = []
         for(var j=0; j<this.size; ++j){
             row.push(((Math.random()*100)>5 ? 1 : 0));
+            // row.push(0);
         }
         this.golArr.push(row);
     }
-    console.log(this.golArr);
 }
 
 GameOfLife.prototype.writeToScreen = function(){
@@ -49,21 +43,57 @@ GameOfLife.prototype.readFromScreen = function(){
     var grid = document.getElementById('game-of-life');
     var rows = grid.children;
     for(var i=0; i<rows.length; ++i){
-        var row = rows[i];
+        var row = rows[i].children;
         for (var j=0 ; j< row.length ; ++j){
             var column = row[j];
-            this.golArr[i][j] = column.className.substring('alive') === -1? 0 : 1;
+            this.golArr[i][j] = column.classList.contains('alive') ? 1 : 0;
         }
     }
-    console.log('read Array :  ',this.golArr)
 }
 
-    
-var gol = new GameOfLife (5);
-// console.log(gol.size);
+GameOfLife.prototype.nextGeneration = function(){
+    /* Find alive neighbours */
+    var tempArr = JSON.parse(JSON.stringify(this.golArr));
+    for(var i=0; i<this.size; ++i){
+        for(var j=0; j<this.size; ++j){
+            var aliveNeighbours = 0;
+            for(var k=-1; k<2; ++k){
+                for(var l=-1; l<2; ++l){
+                    if(!(i+k === -1|| j+l ===-1) && ((i+k)<this.size) && ((j+l)<this.size)){
+                        aliveNeighbours += this.golArr[i+k][j+l];
+                    }
+                }
+            }
+            /* Subtract current element since it's also being added as a neighbour */
+            aliveNeighbours -= this.golArr[i][j];
+            /* Be god */
+            if(aliveNeighbours<2 && this.golArr[i][j]){
+                tempArr[i][j] = 0;
+            }else if((aliveNeighbours===2 || aliveNeighbours ==3)&& this.golArr[i][j]){
+                tempArr[i][j] = 1;
+            }else if((aliveNeighbours>3) && this.golArr[i][j]){
+                tempArr[i][j] = 0;
+            }else if((aliveNeighbours === 3) && !this.golArr[i][j]){
+                tempArr[i][j] = 1
+            }
+        }
+    }
+    this.golArr = JSON.parse(JSON.stringify(tempArr));
+}
+
+var gol = new GameOfLife (20);
 gol.initialize();
 gol.writeToScreen();
-gol.readFromScreen();
-// gol.writeToScreen()
 
+document.getElementById('start-game-of-life').addEventListener('click',()=>{
+    var interval = setInterval(()=>{
+        gol.readFromScreen();
+        gol.nextGeneration();
+        gol.writeToScreen();
+    },100);
+
+    document.getElementById('stop-game-of-life').addEventListener('click',()=>{
+        clearInterval(interval);
+    })
+})
  })();
