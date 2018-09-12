0bce5c47f1523e6fc227a96f8b2ebc1d30f7bdfb
sid <siddhartpai@gmail.com>
2018-09-12 10:38:54 +0530

    Read/write to screen

--- js/script.js
+++ js/script.js
@@ -5,38 +5,65 @@
         [0,1,0],
         [0,0,0]
     ]
-    gameOfLife = (size) =>{
-        return {
-            golArr:[],
-            initialize: ()=>{
-                for(var i=0; i<size; ++i){
-                    for(var j=0; j<size; ++j){
-                        this.golArr[i][j] = 0;
-                    }
+
+function GameOfLife(size){
+    this.size = size;
+    this.golArr = [];
+}
+
+GameOfLife.prototype.initialize = function(){
+    for(var i=0; i<this.size; ++i){
+        var row = []
+        for(var j=0; j<this.size; ++j){
+            row.push(((Math.random()*100)>5 ? 1 : 0));
+        }
+        this.golArr.push(row);
+    }
+    console.log(this.golArr);
+}
+
+GameOfLife.prototype.writeToScreen = function(){
+    document.getElementById('game-of-life').innerHTML = "";
+    this.golArr.map(row=>{
+        var rowDiv = document.createElement('div');
+        rowDiv.className = 'gol-row';
+        row.map( column =>{
+            var columnDiv = document.createElement('div');
+            var className = column? 'alive' : '';
+            className+= ' gol-col';
+            columnDiv.className = className;
+            rowDiv.appendChild(columnDiv)
+            columnDiv.addEventListener('click',(event)=>{
+                if(event.target.classList.contains('alive')){
+                    event.target.classList.remove('alive');
+                }else{
+                    event.target.classList.add('alive');
                 }
-            },
-            readFromScreen: ()=>{
-                
-            },
-            writeToScreen: ()=>{
-                        this.golArr.map(row=>{
-                            var rowDiv = document.createElement('div');
-                            rowDiv.className = 'gol-row';
-                            row.map( column =>{
-                                var columnDiv = document.createElement('div');
-                                var className = column? 'alive' : '';
-                                className+= ' gol-col';
-                                columnDiv.className = className;
-                                rowDiv.appendChild(columnDiv)
-                            });
-                            document.getElementById('game-of-life').appendChild(rowDiv);
-                        });
-            },
-    
+            });
+        });
+        document.getElementById('game-of-life').appendChild(rowDiv);
+    });
+}
+
+GameOfLife.prototype.readFromScreen = function(){
+    var grid = document.getElementById('game-of-life');
+    var rows = grid.children;
+    for(var i=0; i<rows.length; ++i){
+        var row = rows[i];
+        for (var j=0 ; j< row.length ; ++j){
+            var column = row[j];
+            this.golArr[i][j] = column.className.substring('alive') === -1? 0 : 1;
         }
     }
-    var gol = gameOfLife(5);
-    gol.initialize();
-    gol.writeToScreen();
- 
+    console.log('read Array :  ',this.golArr)
+}
+
+    
+var gol = new GameOfLife (5);
+// console.log(gol.size);
+gol.initialize();
+gol.writeToScreen();
+gol.readFromScreen();
+// gol.writeToScreen()
+
  })();
